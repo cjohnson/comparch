a59c4e33987bcf3b23f10bb0998303b3bc218d33
Collin Johnson <collin.g.johnson@gmail.com>
2026-04-06 10:28:29 -0400

    Move RISC-V package into riscv.svh (build order)

--- riscv.svh
+++ riscv.svh
@@ -139,4 +139,45 @@
 // Sign extension for a U-Type instruction.
 `define RV32_U_TYPE_SIGN_EXTEND(instruction) {``instruction``[31:12], {12{1'b0}}}
 
+package riscv;
+  // RISC-V base instruction format structs.
+  //
+  // Volume I: RISC-V Unprivileged ISA Specification
+  // Chapter 2.1.2, Base Instruction Formats
+
+  // Format of an R-Type instruction.
+  typedef struct packed {
+    logic [6:0] funct7;
+    logic [4:0] rs2;
+    logic [4:0] rs1;
+    logic [2:0] funct3;
+    logic [4:0] rd;
+    logic [6:0] opcode;
+  } r_type_instruction_t;
+
+  // Format of an I-Type instruction.
+  typedef struct packed {
+    logic [11:0] imm;
+    logic [4:0]  rs1;
+    logic [2:0]  funct3;
+    logic [4:0]  rd;
+    logic [6:0]  opcode;
+  } i_type_instruction_t;
+
+  // Format of a U-Type instruction.
+  typedef struct packed {
+    logic [19:0] imm;
+    logic [4:0]  rd;
+    logic [6:0]  opcode;
+  } u_type_instruction_t;
+
+  // A union of the RISC-V instruction types.
+  typedef union packed {
+    logic [31:0] data;
+    r_type_instruction_t r_type;
+    i_type_instruction_t i_type;
+    u_type_instruction_t u_type;
+  } instruction_t;
+endpackage : riscv
+
 `endif  // RISCV_SVH_
